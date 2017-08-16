2650df502471a713a85a1143f9821f4e9a4175e1
Ferdy Budhidharma <ferd312@gmail.com>
2017-08-15 20:29:54 -0500

    FInished exercise

--- main.ts
+++ main.ts
@@ -1,28 +1,47 @@
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/map';
-
-import { Observable } from 'rxjs/Observable';
+import { Observable } from "rxjs";
 
 const numbers = [1, 5, 10];
-const source = new Observable<number>(obs => {
-    let idx = 0;
-    const produceValue = () => {
-        obs.next(numbers[idx++]);
+const [output, button] = [document.getElementById("output"), document.getElementById("button")];
+let clickStream: Observable<MouseEvent> = new Observable();
+if (button) {
+    clickStream = Observable.fromEvent<MouseEvent>(button, "click");
+}
 
-        if (idx < numbers.length) {
-            setTimeout(produceValue, 250);
-        } else {
-            obs.complete();
-        }
-    };
+const load = (url: string) => {
+    return Observable.defer(() => fetchMovies(url)).retryWhen(retryObservable());
+    // return new Observable<Array<{ title: string }>>(obs => {
+    //     fetchMovies(url).then(movieList => (obs.next(movieList), obs.complete())).catch(err => obs.error(err));
+    // }).retryWhen(retryObservable);
+};
 
-    produceValue();
-})
-    .map(n => n * 2)
-    .filter(n => n > 2);
+const retryObservable = (maxRetries: number = 5, delayMs: number = 1000) => (errObs: Observable<any>) =>
+    errObs
+        .scan<number>((acc, val) => {
+            acc++;
+            if (acc < maxRetries) {
+                return acc;
+            } else {
+                throw val;
+            }
+        }, 0)
+        .delay(delayMs);
 
-source.subscribe(
-    val => console.log(`Value: ${val}`),
-    err => console.log(`Error: ${err}`),
-    () => console.log(`Completed!`)
-);
+const fetchMovies = (url: string) =>
+    fetch(url)
+        .then(res => (res.status === 200 ? res.text() : Promise.reject(`Server returned ${res.status}`)))
+        .then(txt => JSON.parse(txt) as Array<{ title: string }>);
+
+const renderMovies = (m: { title: string }) => {
+    const div = document.createElement("div");
+    div.innerText = m.title;
+    output && output.appendChild(div);
+};
+
+const logError = (err: any) => console.log(`Error: ${err}`);
+
+const logComplete = () => console.log(`Completed!`);
+
+clickStream
+    .flatMap(val => load("moviess.json"))
+    .flatMap(movieList => Observable.from(movieList))
+    .subscribe(renderMovies, logError, logComplete);
